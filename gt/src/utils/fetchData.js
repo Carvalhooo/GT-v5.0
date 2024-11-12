a651cf67bf5c5892860ed4a1dbe19cf4eae35d8d
Carvalhooo <lucasdemelocarvalho2@gmail.com>
2024-11-12 13:40:48 -0300

    GT-v5.0

---
+++ gt/src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions ={
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'c9893783femshd75dca53b66f6e8p12994bjsn84262063433e',
+  }
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'c9893783femshd75dca53b66f6e8p12994bjsn84262063433e',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
